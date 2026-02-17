1c44a1cbb6ce65e041a97c706713298d08422fa5
Jesus Manuel Bobadilla <aldinhobobadilla@gmail.com>
2026-02-17 15:50:36 -0600

    Se agrega la sincronia de cada 60 segundos de actualizacion

--- src/index.js
+++ src/index.js
@@ -3,14 +3,39 @@ import { GROUP_FILTER } from "./config/guzman.config.js";
 import { mapGuzman } from "./services/guzman/guzman.index.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
+  const initWialon = async () => {
   try {
-    await WialonService.login( TOKEN_WIALON );
-    
-    const groups_with_units = await WialonService.loadGroupsWithUnits( GROUP_FILTER );
+    await WialonService.login(TOKEN_WIALON);
 
-    mapGuzman( groups_with_units )
+    const groups_with_units =
+      await WialonService.loadGroupsWithUnits(GROUP_FILTER);
+
+    mapGuzman(groups_with_units);
 
   } catch (err) {
     console.error(err);
   }
+};
+
+const relouder = async () => {
+  try {
+    console.log('Recargando informacion');
+    
+    const groups_with_units =
+      await WialonService.loadGroupsWithUnits(GROUP_FILTER);
+
+    mapGuzman(groups_with_units);
+
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Ejecutar login + primera carga
+await initWialon();
+
+// Repetir cada 50 segundos
+setInterval(relouder, 50 * 1000);
+
+
 });
