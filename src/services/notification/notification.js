5be7916d4d80736b55768d3cf28bef5830c03a91
Jesus Manuel Bobadilla <aldinhobobadilla@gmail.com>
2026-07-15 16:53:50 -0600

    hotfix: correcion de pats y logs

--- src/services/notification/notification.js
+++ src/services/notification/notification.js
@@ -1,8 +1,7 @@
 export async function sendNotificationDataBase(notification) {
-    console.log( notification );
-    
   try {
     const response = await fetch("http://ws4cjdg.com/JDigitalReportsV2/src/api/routes/notifications/addNotification.php", {
+    // const response = await fetch("http://localhost:8080/repos/Jornada%20Digital/JornadaDigital.ReportesDeUnidades.com/src/api/routes/notifications/addNotification.php", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
